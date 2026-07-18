37c22071286eb413277be37c89fd4c8702f0663e
Heriyadi <adipatikanjeng@gmail.com>
2026-07-18 10:08:54 +0700

    chore: add res.cloudinary.com to images remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'commons.wikimedia.org' },
       { protocol: 'https', hostname: 'tourismdev.id' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
